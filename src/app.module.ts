74e7d229046f0f8bcbde0bfa4f93af910af69cf9
Yancarlo J. Urdaneta G. <>
2025-04-29 14:32:51 -0400

    add city event type module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { CountriesModule } from './countries/countries.module';
+import { StatesModule } from './states/states.module';
+import { CitiesModule } from './cities/cities.module';
+import { EventTypesModule } from './event-types/event-types.module';
+import { LeadsModule } from './leads/leads.module';
+import { CityEventTypesModule } from './city-event-types/city-event-types.module';
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CountriesModule, StatesModule, CitiesModule, EventTypesModule, LeadsModule, CityEventTypesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
