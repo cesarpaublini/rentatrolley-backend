3dca919a6d18430973f8671c0c6d883ca357c8bc
Yancarlo J. Urdaneta G. <>
2025-04-30 10:10:18 -0400

    add description to leads swagger

--- src/leads/dto/create-lead.dto.ts
+++ src/leads/dto/create-lead.dto.ts
@@ -2,20 +2,24 @@
 
 // eslint-disable-next-line prettier/prettier
 import { IsBoolean, IsDateString, IsEmail, IsNotEmpty, IsNumber, IsString } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateLeadDto {
+  @ApiProperty({ description: 'The first name of the lead' })
   @IsNotEmpty()
   @IsString()
   first_name: string;
 
+  @ApiProperty({ description: 'The last name of the lead' })
   @IsNotEmpty()
   @IsString()
   last_name: string;
 
+  @ApiProperty({ description: 'The email of the lead' })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({ description: 'The phone number of the lead' })
   @IsNotEmpty()
   @IsString()
   phone_number: string;
@@ -28,6 +32,7 @@ export class CreateLeadDto {
   @IsNumber()
   event_type_id: number;
 
+  @ApiProperty({ description: 'The event date of the lead' })
   @IsNotEmpty()
   @IsDateString()
   event_date: Date;
@@ -40,12 +45,14 @@ export class CreateLeadDto {
   @IsNumber()
   drop_city_id: number;
 
+  @ApiProperty({ description: 'The pickup date and time of the lead' })
   @IsNotEmpty()
   @IsDateString()
   pickup_date_time: Date;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty({ description: 'The duration of the trip in hours' })
   duration_hours: number;
 
   @IsNotEmpty()
@@ -54,13 +61,16 @@ export class CreateLeadDto {
 
   @IsNotEmpty()
   @IsBoolean()
+  @ApiProperty({ description: 'Whether the trip is a return trip' })
   return_trip: boolean;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty({ description: 'The number of passengers' })
   passenger_count: number;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ description: 'Any special requirements for the trip' })
   special_requirements: string;
 }
