127d68891213c74dbc620390abaadd87bd46d0e5
Yancarlo J. Urdaneta G. <>
2025-04-30 21:56:18 -0400

    swagger doc for cities

--- src/cities/dto/create-city.dto.ts
+++ src/cities/dto/create-city.dto.ts
@@ -1,11 +1,19 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCityDto {
+  @ApiProperty({
+    description: 'The name of the city',
+    example: 'New York',
+  })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({
+    description: 'The state ID of the city',
+    example: 1,
+  })
   @IsNotEmpty()
   @IsNumber()
   state_id: number;
--- src/cities/dto/update-city.dto.ts
+++ src/cities/dto/update-city.dto.ts
@@ -1,4 +1,19 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCityDto } from './create-city.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateCityDto extends PartialType(CreateCityDto) {}
+export class UpdateCityDto extends PartialType(CreateCityDto) {
+  @ApiProperty({
+    description: 'The name of the city',
+    example: 'New York',
+    required: false,
+  })
+  name?: string;
+
+  @ApiProperty({
+    description: 'The state ID of the city',
+    example: 1,
+    required: false,
+  })
+  state_id?: number;
+}
