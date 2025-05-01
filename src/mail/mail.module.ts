d02964697b10874f18ddc3aaab913db12b911370
Yancarlo J. Urdaneta G. <>
2025-05-01 11:17:05 -0400

    add mail service

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,7 +1,40 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
-
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
+import * as path from 'path';
 @Module({
+  imports: [
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.get('SMTP_HOST'),
+          port: +configService.get('SMTP_PORT'),
+          secure: true,
+          auth: {
+            user: configService.get<string>('SMTP_USER'),
+            pass: configService.get<string>('SMTP_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: configService.get('FROM'),
+          replyTo: configService.get('REPLY_TO'),
+          bcc: configService.get('BCC'),
+        },
+        template: {
+          dir: path.join(path.dirname(__dirname), 'templates'),
+          adapter: new PugAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [MailService],
   exports: [MailService],
 })
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { ISendMailOptions, MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  constructor(private readonly mailerService: MailerService) {}
+
+  async sendEmail(
+    to: string,
+    subject: string,
+    text: string,
+    context: ISendMailOptions['context'],
+  ) {
+    await this.mailerService.sendMail({
+      to,
+      subject,
+      text,
+      template: 'welcome',
+      context,
+    });
+  }
+}
