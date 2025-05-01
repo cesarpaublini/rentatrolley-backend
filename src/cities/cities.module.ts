8baa944b037a4ffbdf98a29470056975bc0e6a61
Yancarlo J. Urdaneta G. <>
2025-04-30 22:56:44 -0400

    add cities crud functionality

--- src/cities/cities.module.ts
+++ src/cities/cities.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CitiesService } from './cities.service';
 import { CitiesController } from './cities.controller';
+import { City } from './entities/city.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([City])],
   controllers: [CitiesController],
   providers: [CitiesService],
 })
--- src/cities/cities.service.ts
+++ src/cities/cities.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCityDto } from './dto/create-city.dto';
 import { UpdateCityDto } from './dto/update-city.dto';
+import { City } from './entities/city.entity';
+import { Repository, UpdateResult, DeleteResult } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class CitiesService {
+  constructor(
+    @InjectRepository(City)
+    private cityRepository: Repository<City>,
+  ) {}
   create(createCityDto: CreateCityDto) {
-    return 'This action adds a new city';
+    return this.cityRepository.save(createCityDto);
   }
 
-  findAll() {
-    return `This action returns all cities`;
+  findAll(): Promise<City[]> {
+    return this.cityRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} city`;
+  findOne(id: number): Promise<City | null> {
+    return this.cityRepository.findOneBy({ id });
   }
 
-  update(id: number, updateCityDto: UpdateCityDto) {
-    return `This action updates a #${id} city`;
+  update(id: number, updateCityDto: UpdateCityDto): Promise<UpdateResult> {
+    return this.cityRepository.update(id, updateCityDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} city`;
+  remove(id: number): Promise<DeleteResult> {
+    return this.cityRepository.delete(id);
   }
 }
