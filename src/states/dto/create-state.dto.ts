7424e3ce4f80ced2f4d507e6df03a9f1e80e311f
Yancarlo J. Urdaneta G. <>
2025-04-30 21:55:24 -0400

    add swagger docs for states

--- src/states/dto/create-state.dto.ts
+++ src/states/dto/create-state.dto.ts
@@ -1,15 +1,27 @@
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 export class CreateStateDto {
+  @ApiProperty({
+    description: 'The name of the state',
+    example: 'California',
+  })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({
+    description: 'The ISO code of the state',
+    example: 'CA',
+  })
   @IsNotEmpty()
   @IsString()
   abbreviation: string;
 
+  @ApiProperty({
+    description: 'The country ID to which the state belongs',
+    example: 1,
+  })
   @IsNotEmpty()
   @IsNumber()
   country_id: number;
--- src/states/dto/update-state.dto.ts
+++ src/states/dto/update-state.dto.ts
@@ -1,4 +1,26 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateStateDto } from './create-state.dto';
 
-export class UpdateStateDto extends PartialType(CreateStateDto) {}
+export class UpdateStateDto extends PartialType(CreateStateDto) {
+  @ApiProperty({
+    description: 'The name of the state',
+    example: 'California',
+    required: false,
+  })
+  name?: string;
+
+  @ApiProperty({
+    description: 'The ISO code of the state',
+    example: 'CA',
+    required: false,
+  })
+  iso_code?: string;
+
+  @ApiProperty({
+    description: 'The country ID to which the state belongs',
+    example: 1,
+    required: false,
+  })
+  country_id?: number;
+}
