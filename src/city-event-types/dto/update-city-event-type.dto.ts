f27f28cb5e2b2ba8bd07addbaa3ab3be3d8586db
Yancarlo J. Urdaneta G. <>
2025-04-30 21:58:08 -0400

    city event type swagger docs

--- src/city-event-types/dto/update-city-event-type.dto.ts
+++ src/city-event-types/dto/update-city-event-type.dto.ts
@@ -1,4 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCityEventTypeDto } from './create-city-event-type.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateCityEventTypeDto extends PartialType(CreateCityEventTypeDto) {}
+export class UpdateCityEventTypeDto extends PartialType(
+  CreateCityEventTypeDto,
+) {
+  @ApiProperty({
+    description: 'The city ID of the event type',
+    example: 1,
+    required: false,
+  })
+  city_id?: number;
+
+  @ApiProperty({
+    description: 'The event type ID of the city event type',
+    example: 1,
+    required: false,
+  })
+  event_type_id?: number;
+}
