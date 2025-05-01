991a2eb7bfba9d19ac6f5fafdb5cd56f1cb4ab77
Yancarlo J. Urdaneta G. <>
2025-04-30 21:57:09 -0400

    add swagger docs for event types

--- src/event-types/dto/create-event-type.dto.ts
+++ src/event-types/dto/create-event-type.dto.ts
@@ -1,7 +1,11 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateEventTypeDto {
+  @ApiProperty({
+    description: 'The name of the event type',
+    example: 'Trolley Ride',
+  })
   @IsNotEmpty()
   @IsString()
   name: string;
