8df46934376f70f72534474da7f659113a292e5a
Yancarlo J. Urdaneta G. <>
2025-04-30 21:52:52 -0400

    add swagger docs por countries

--- src/countries/dto/create-country.dto.ts
+++ src/countries/dto/create-country.dto.ts
@@ -1,15 +1,24 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCountryDto {
+  @ApiProperty({
+    description: 'The name of the country',
+    example: 'United States',
+  })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({ description: 'The ISO code of the country', example: 'US' })
   @IsNotEmpty()
   @IsString()
   iso_code: string;
 
+  @ApiProperty({
+    description: 'The currency ISO code of the country',
+    example: 'USD',
+  })
   @IsNotEmpty()
   @IsString()
   currency_iso: string;
--- src/countries/dto/update-country.dto.ts
+++ src/countries/dto/update-country.dto.ts
@@ -1,4 +1,26 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateCountryDto } from './create-country.dto';
 
-export class UpdateCountryDto extends PartialType(CreateCountryDto) {}
+export class UpdateCountryDto extends PartialType(CreateCountryDto) {
+  @ApiProperty({
+    description: 'The name of the country',
+    example: 'United States',
+    required: false,
+  })
+  name?: string;
+
+  @ApiProperty({
+    description: 'The ISO code of the country',
+    example: 'US',
+    required: false,
+  })
+  iso_code?: string;
+
+  @ApiProperty({
+    description: 'The currency ISO code of the country',
+    example: 'USD',
+    required: false,
+  })
+  currency_iso?: string;
+}
