cfdde3709be33a02324e2bffa95787b053c7aeb3
Yancarlo J. Urdaneta G. <>
2025-05-18 18:49:54 -0400

    change hardcode template to enums

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ISendMailOptions, MailerService } from '@nestjs-modules/mailer';
+import { MailTemplates } from 'src/utils/mail-templates';
 
 @Injectable()
 export class MailService {
@@ -9,13 +10,14 @@ export class MailService {
     to: string,
     subject: string,
     text: string,
+    template: MailTemplates,
     context: ISendMailOptions['context'],
   ) {
     await this.mailerService.sendMail({
       to,
       subject,
       text,
-      template: 'welcome',
+      template,
       context,
     });
   }
