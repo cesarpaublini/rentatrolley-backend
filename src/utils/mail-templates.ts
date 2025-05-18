23e60562c42ff3f8c87bdae8e437a86408660feb
Yancarlo J. Urdaneta G. <>
2025-05-18 18:45:16 -0400

    template enums

---
+++ src/utils/mail-templates.ts
@@ -0,0 +1,9 @@
+export enum MailTemplates {
+  WELCOME = 'welcome',
+  CONTACT_US = 'contact-us',
+}
+
+export enum MailSubjects {
+  WELCOME = 'Welcome to Rent a Trolley',
+  CONTACT_US = 'Contact Us',
+}
