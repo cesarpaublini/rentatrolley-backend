75382275730474e31788ef22db6cba70189867ef
Yancarlo J. Urdaneta G. <>
2025-05-19 10:39:59 -0400

    import event types

--- src/stripe/stripe.module.ts
+++ src/stripe/stripe.module.ts
@@ -4,9 +4,16 @@ import { StripeService } from './stripe.service';
 import { StripeWebhookController } from './stripe-webhook.controller';
 import { CustomersModule } from 'src/customers/customers.module';
 import { LeadsModule } from 'src/leads/leads.module';
+import { MailService } from 'src/mail/mail.service';
+import { EventTypesModule } from 'src/event-types/event-types.module';
 @Module({
-  imports: [ConfigModule, forwardRef(() => LeadsModule), CustomersModule],
-  providers: [StripeService],
+  imports: [
+    ConfigModule,
+    forwardRef(() => LeadsModule),
+    CustomersModule,
+    forwardRef(() => EventTypesModule),
+  ],
+  providers: [StripeService, MailService],
   exports: [StripeService],
   controllers: [StripeWebhookController],
 })
