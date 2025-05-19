8b398367a4fea8973f105eb16965efc532b9bee8
Yancarlo J. Urdaneta G. <>
2025-05-19 10:38:37 -0400

    add purchase template

--- src/utils/mail-templates.ts
+++ src/utils/mail-templates.ts
@@ -1,9 +1,11 @@
 export enum MailTemplates {
   WELCOME = 'welcome',
   CONTACT_US = 'contact-us',
+  PURCHASE = 'purchase',
 }
 
 export enum MailSubjects {
   WELCOME = 'Welcome to Rent a Trolley',
   CONTACT_US = 'Contact Us',
+  PURCHASE = 'Purchase Confirmation',
 }
